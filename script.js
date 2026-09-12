643f4e6bc3679b63511b16904478c2b492e22fd9
Julia <juliafcostads@gmail.com>
2026-09-11 22:01:21 -0300

    feat: fecha caixa de resultado ao remover opção

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const opcoes = []; //lista vazia para receber as opções digitadas pelo usuário
+const opcoes = []; //armazena as opções digitadas pelo usuário
 const itens = []; //para gerenciar as opções visíveis em lista
 
 //captura dos elementos html
@@ -7,77 +7,80 @@ const botaoAdd = document.getElementById("botao-add");
 const botaoGirar = document.getElementById("botao-girar");
 const sorteado = document.getElementById("resultado");
 const listagem = document.getElementById("listagem");
-const itemLista = document.createElement("li");
-const botaoRemover = document.getElementById("botao-remover")
+const botaoRemover = document.getElementById("botao-remover");
+const caixaDestaque = document.getElementById("caixa-destaque");
+const botaoFechar = document.getElementById("botao-fechar");
 
 let indice = -1; //atribui um estado absurdo equivalente há "indice sem alterações, sem sorteio ainda"
 
-function adicionarOpcao(){
+caixaDestaque.style.display = "none";
 
-    const opcao = entrada.value.trim(); //guarda o texto limpo de espaços externos
-    
-    //validação do texto inserido
-    if(opcao === ""){
-        alert("O campo não pode estar vazio!");
+function adicionarOpcao() {
+  const opcao = entrada.value.trim(); //guarda o texto limpo de espaços externos
 
-    } else {
+  //validação do texto inserido
+  if (opcao === "") {
+    alert("O campo não pode estar vazio!");
+  } else {
+    //armazena o texto inserido no array
+    opcoes.push(opcao);
 
-        //armazena o texto inserido no array
-        opcoes.push(opcao);
+    const item = document.createElement("li"); //cria um item para a lista
 
-        const item = document.createElement("li"); //cria um item para a lista
+    item.textContent = opcao; //adiciona ao item da lista o dado inserido
+    listagem.appendChild(item); //adiciona esse item a lista
 
-        item.textContent = opcao; //adiciona ao item da lista o dado inserido
-        listagem.appendChild(item); //adiciona esse item a lista
+    itens.push(item);
 
-        itens.push(item);
-
-        entrada.value = "";
-    }
+    entrada.value = "";
+  }
 }
 
-botaoAdd.addEventListener("click", function(){
+botaoAdd.addEventListener("click", function () {
+  adicionarOpcao();
+});
+
+//adiciona o texto digitado no input aos arrays quando pressionar Enter
+entrada.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
     adicionarOpcao();
+  }
 });
 
-//adiciona o texto digitado no input ao array quando pressionar Enter
-entrada.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        adicionarOpcao();
-    }
-});
-
-botaoGirar.addEventListener("click", function() {
-
-    if(!opcoes.length){
-        alert("Sem opções a serem sortadas!")
-
-    } else {
-        //guarda no indice apenas o inteiro do número decimal resultante do cálculo
+botaoGirar.addEventListener("click", function () {
+  if (!opcoes.length) {
+    alert("Sem opções a serem sortadas!");
+  } else {
+    //guarda no indice apenas o inteiro do número decimal resultante do cálculo
     indice = Math.floor(Math.random() * opcoes.length);
 
     const resultado = opcoes[indice];
     //guarda o valor referente ao índice
 
     sorteado.textContent = resultado;
-    }
+
+    caixaDestaque.style.display = "block";
+  }
 });
 
-botaoRemover.addEventListener("click", function(){
+botaoRemover.addEventListener("click", function () {
+  //antes de remover valida se a lista de opções está vazia e se não houve sorteio
 
-    //antes de remover valida se a lista de opções está vazia e se não houve sorteio
-    if(!opcoes.length || (indice === -1) ){
-        alert("Sem opções para remover!");
-
-    } else {
+  if (!opcoes.length || indice === -1) {
+    alert("Sem opções para remover!");
+  } else {
     itens[indice].remove(); //remove da lista visível a opção sorteada
-    
-    //remove o sorteado dos arrays
-    opcoes.splice(indice, 1);
-    itens.splice(indice, 1);
 
-    sorteado.textContent = ""; //para limpar o resultado quando remover a opção
-    }
+    //remove o sorteado dos arrays
+    itens.splice(indice, 1);
+    opcoes.splice(indice, 1);
+
+    caixaDestaque.style.display = "none";
+  }
+});
+
+botaoFechar.addEventListener("click", function () {
+  caixaDestaque.style.display = "none";
 });
 
 /*sobre o cáculo:
